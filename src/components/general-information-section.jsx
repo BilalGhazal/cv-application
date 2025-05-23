5f01e9a5d1be67ce5f6fd4650b817158db5e278c
Bilal Ghazal <bilalghazal98@gmail.com>
2025-05-23 05:36:09 +0300

    Finish first stage of general information section

---
+++ src/components/general-information-section.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+function GeneralInformation() {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [phoneNumber, setPhoneNumber] = useState("")
+
+    return (
+        <>
+            <fieldset>
+                <legend>
+                    Personal Information:
+                </legend>
+                <div>
+                    <label for="name">Name:</label>
+                    <input type="text" id="name" onChange={(e) => setName(e.target.value)} />
+                </div>
+
+                <div>
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" onChange={(e) => setEmail(e.target.value)} />
+                </div>
+
+                <div>
+                    <label for="phone-number">Phone Number:</label>
+                    <input type="tel" id="phone-number" onChange={(e) => setPhoneNumber(e.target.value)} />
+                </div>
+
+            </fieldset>
+        </>
+    )
+}
+
+
+export default GeneralInformation
